834a14b3e1e8eaff3c61c81f75ffaa6a7931ac75
minsuKang <minsu910725@gmail.com>
2022-05-19 11:08:05 +0900

    Add:DeleteToDo

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,33 +1,42 @@
-import { legacy_createStore as createStore } from 'redux';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store';
+import ToDo from '../components/ToDo';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-export const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo !== action.id);
-    default:
-      return state;
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState('');
+  function onChange(e) {
+    setText(e.target.value);
   }
-};
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText('');
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
 
-const store = createStore(reducer);
+function mapStateToProps(state) {
+  return { toDos: state };
+}
 
-export default store;
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
